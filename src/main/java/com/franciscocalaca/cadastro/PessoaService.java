87979f6ccd8b746a807a4fd7dc82c4b473f36191
Francisco Calaca <chicocx@gmail.com>
2019-02-21 11:19:46 -0300

    inclusao dos servicos /save e /delete

--- src/main/java/com/franciscocalaca/cadastro/PessoaService.java
+++ src/main/java/com/franciscocalaca/cadastro/PessoaService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +25,28 @@ public class PessoaService {
 		return result;
 	}
 	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public void save(@RequestBody Pessoa pessoa) {
+		pessoaDao.save(pessoa);
+	}
+	
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") Integer id) {
+		pessoaDao.deleteById(id);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
